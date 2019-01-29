a5aefff461b8b0e700fe394e086d38ddf00385eb
Neelansh <sethineelansh@gmail.com>
2019-01-30 03:25:31 +0530

    Updtaed libraries!

--- app/src/main/java/com/example/neelanshsethi/sello/FragmentSettings.java
+++ app/src/main/java/com/example/neelanshsethi/sello/FragmentSettings.java
@@ -1,7 +1,18 @@
 package com.example.neelanshsethi.sello;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 
@@ -11,16 +22,10 @@ public class FragmentSettings extends PreferenceFragmentCompat {
         // Required empty public constructor
     }
 
-
-
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_preferences);
-
-
     }
-
-
 
     @Override
     public void onDetach() {
