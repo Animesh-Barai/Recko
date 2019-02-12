3ed98b2996c1811e04040a777668c28876935f03
Khagesh Patel <khageshpatel93@gmail.com>
2019-02-13 01:02:40 +0530

    added refer screen

---
+++ app/src/main/java/com/example/neelanshsethi/recko/ReferActivity.java
@@ -0,0 +1,15 @@
+package com.example.neelanshsethi.recko;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReferActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer);
+    }
+
+}
