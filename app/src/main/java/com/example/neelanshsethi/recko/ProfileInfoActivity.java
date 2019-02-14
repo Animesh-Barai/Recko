02e947186e6f8a00458337841b3000460f86ebf8
Khagesh Patel <khageshpatel93@gmail.com>
2019-02-15 01:52:07 +0530

    some more functionality

--- app/src/main/java/com/example/neelanshsethi/recko/ProfileInfoActivity.java
+++ app/src/main/java/com/example/neelanshsethi/recko/ProfileInfoActivity.java
@@ -1,13 +1,111 @@
 package com.example.neelanshsethi.recko;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.Wave;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileInfoActivity extends AppCompatActivity {
+    TextView seller_name;
+    TextView seller_mobile_no;
+    TextView seller_location;
+    ProgressBar progressBar;
+    androidx.appcompat.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_details);
+        seller_name = findViewById(R.id.seller_name);
+        seller_mobile_no = findViewById(R.id.seller_mobile_no);
+        seller_location = findViewById(R.id.seller_location);
+        progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        Sprite wave = new Wave();
+        progressBar.setIndeterminateDrawable(wave);
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        fetch_details();
+    }
 
+    private void fetch_details() {
+
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("seller_uuid", mUser != null ? mUser.getUid() : null);
+            Log.d("zzz MainActivity json", json.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        seller_mobile_no.setText(mUser.getPhoneNumber());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,APIURL.url+"user/query", json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("zzz MainActivity", APIURL.url + "user/query" + "\nonResponse: " + response);
+
+                        try {
+                            String code = response.getString("code");
+                            if (code.equals("200")) {
+                                JSONObject data = response.getJSONObject("data");
+                                String location = "";
+                                String name = "";
+                                try {
+                                    location = data.getString("seller_location");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    name =  data.getString("seller_name");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                seller_name.setText(name);
+                                seller_location.setText(location);
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Oops! Please try again later",Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
     }
 }
