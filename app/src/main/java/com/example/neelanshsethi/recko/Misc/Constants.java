9be7a7479c51ffd7c95bad80baa6e9a9ed83ea37
Khagesh Patel <khageshpatel93@gmail.com>
2019-02-20 23:37:34 +0530

    user info screen

--- app/src/main/java/com/example/neelanshsethi/recko/Misc/Constants.java
+++ app/src/main/java/com/example/neelanshsethi/recko/Misc/Constants.java
@@ -1,6 +1,12 @@
 package com.example.neelanshsethi.recko.Misc;
 
+import android.util.Log;
+import android.util.Patterns;
+
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Constants {
     public static final int max_company_height_in_explore = 300;
@@ -8,7 +14,18 @@ public class Constants {
     public static final String edit_lead_title = "Edit Lead";
     public static final int edit_active_lead_request_id = 100;
     public static final int edit_missed_lead_request_id = 101;
+    public static final int edit_user_info_request_id = 102;
+
     public static boolean isValidURL(String url) {
         return !StringUtils.isEmpty(url) && !url.equals("null");
+    }
+
+    public static String fixNullString(String text) {
+        if (text.equals("null")) return "";
+        return text;
+    }
+
+    public static boolean validateEmail(String emailStr) {
+        return (!emailStr.trim().equals("") && Patterns.EMAIL_ADDRESS.matcher(emailStr).matches());
     }
 }
--- app/src/main/java/com/example/neelanshsethi/recko/ProfileInfoActivity.java
+++ app/src/main/java/com/example/neelanshsethi/recko/ProfileInfoActivity.java
@@ -1,9 +1,16 @@
 package com.example.neelanshsethi.recko;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +22,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.neelanshsethi.recko.Misc.Constants;
+import com.example.neelanshsethi.recko.Model.ManageLeadsModel;
 import com.github.ybq.android.spinkit.sprite.Sprite;
 import com.github.ybq.android.spinkit.style.Wave;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,14 +34,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileInfoActivity extends AppCompatActivity {
     TextView seller_name;
     TextView seller_mobile_no;
+    TextView seller_email;
     TextView seller_location;
     ProgressBar progressBar;
     androidx.appcompat.widget.Toolbar toolbar;
+    ImageView edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +53,7 @@ public class ProfileInfoActivity extends AppCompatActivity {
         seller_name = findViewById(R.id.seller_name);
         seller_mobile_no = findViewById(R.id.seller_mobile_no);
         seller_location = findViewById(R.id.seller_location);
+        seller_email = findViewById(R.id.seller_email);
         progressBar = (ProgressBar)findViewById(R.id.spin_kit);
         Sprite wave = new Wave();
         progressBar.setIndeterminateDrawable(wave);
@@ -51,7 +64,27 @@ public class ProfileInfoActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        edit = findViewById(R.id.edit);
         fetch_details();
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),EditUserInfoActivity.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("seller_name", seller_name.getText().toString());
+                intent.putExtra("seller_mobile_no", seller_mobile_no.getText().toString());
+                intent.putExtra("seller_email", seller_email.getText().toString());
+                intent.putExtra("seller_location", seller_location.getText().toString());
+                startActivityForResult(intent, Constants.edit_user_info_request_id);
+                //intent.putExtra("edit_lead_model", manageLeadsModel);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //if (fragment != null)
+                //    fragment.startActivityForResult(intent, Constants.edit_missed_lead_request_id);
+                //else
+                //    ((Activity)mctx).startActivityForResult(intent, Constants.edit_missed_lead_request_id);
+            }
+        });
     }
 
     private void fetch_details() {
@@ -71,7 +104,7 @@ public class ProfileInfoActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        Log.d("zzz MainActivity", APIURL.url + "user/query" + "\nonResponse: " + response);
+                        Log.d("zzz ProfileInfoActivity", APIURL.url + "user/query" + "\nonResponse: " + response);
 
                         try {
                             String code = response.getString("code");
@@ -79,6 +112,7 @@ public class ProfileInfoActivity extends AppCompatActivity {
                                 JSONObject data = response.getJSONObject("data");
                                 String location = "";
                                 String name = "";
+                                String email = "";
                                 try {
                                     location = data.getString("seller_location");
                                 } catch (JSONException e) {
@@ -89,8 +123,19 @@ public class ProfileInfoActivity extends AppCompatActivity {
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
+                                try {
+                                    Log.d("zzz profile info act", email);
+                                    email = data.getString("seller_email");
+                                    Log.d("zzz profile info act1", email);
+                                    email = Constants.fixNullString(email);
+                                    Log.d("zzz profile info act2", email);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                Log.d("zzz profile info act", email);
                                 seller_name.setText(name);
                                 seller_location.setText(location);
+                                seller_email.setText(email);
                                 progressBar.setVisibility(View.GONE);
                             }
                         } catch (JSONException e) {
@@ -107,5 +152,19 @@ public class ProfileInfoActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("zzz profile info act", "get called");
+        if(resultCode==Activity.RESULT_OK)
+        {
+            if (requestCode == Constants.edit_user_info_request_id) {
+                seller_email.setText(data.getStringExtra("seller_email"));
+                seller_location.setText(data.getStringExtra("seller_location"));
+            }
+
+        }
     }
 }
