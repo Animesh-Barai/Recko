41c8ffad309d5f216ef42da77d9cb9c13f2df909
Khagesh Patel <khageshpatel93@gmail.com>
2019-02-23 00:03:30 +0530

    logout

--- app/src/main/java/com/example/neelanshsethi/recko/VerifyPhoneNumber.java
+++ app/src/main/java/com/example/neelanshsethi/recko/VerifyPhoneNumber.java
@@ -14,6 +14,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
@@ -27,6 +33,11 @@ import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.raycoarana.codeinputview.CodeInputView;
 import com.raycoarana.codeinputview.OnCodeCompleteListener;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -208,6 +219,104 @@ public class VerifyPhoneNumber extends AppCompatActivity {
         signInWithPhoneAuthCredential(credential);
     }
 
+    private void navigateToUserInfo() {
+        Intent intent = new Intent(VerifyPhoneNumber.this, UserInfo.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private void goto_navigation() {
+        Intent intent=new Intent(this,NavigationDashboard.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private void fetch_details() {
+
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("seller_uuid", mUser != null ? mUser.getUid() : null);
+            Log.d("zzz MainActivity json", json.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,APIURL.url+"user/query", json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("zzz MainActivity", APIURL.url + "user/query" + "\nonResponse: " + response);
+
+                        try {
+                            String code = response.getString("code");
+                            if (code.equals("200")) {
+                                JSONObject data = response.getJSONObject("data");
+                                String location = "";
+                                String name = "";
+                                try {
+                                    location = data.getString("seller_location");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    name =  data.getString("seller_name");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                if (StringUtils.isAnyEmpty(name, location)){
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                    bundle.putInt("quit_before_user_info", 1);
+                                    mFirebaseAnalytics.logEvent("quit_before_user_info", bundle);
+
+                                    // We don't have name/location send him/her to UserInfo page.
+                                    navigateToUserInfo();
+                                    return;
+                                } else {
+                                    JSONArray industries = null;
+                                    try {
+                                        industries =  data.getJSONArray("industries");
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                        mFirebaseAnalytics.logEvent("relogin", bundle);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    if (industries!=null && industries.length() > 0) {
+                                        goto_navigation();
+                                    } else {
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                        mFirebaseAnalytics.logEvent("quit_before_industries", bundle);
+
+                                        // We don't have industry list for user send him/her to pick industries page.
+                                        Intent intent = new Intent(getApplicationContext(), Industries.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Oops! Please try again later",Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(VerifyPhoneNumber.this, new OnCompleteListener<AuthResult>() {
@@ -219,12 +328,7 @@ public class VerifyPhoneNumber extends AppCompatActivity {
                             Bundle bundle = new Bundle();
                             bundle.putString("user_id", user.getUid());
                             mFirebaseAnalytics.logEvent("logged_in_or_signup", bundle);
-                            //verification successful we will start the profile activity
-                            Intent intent = new Intent(VerifyPhoneNumber.this, UserInfo.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(intent);
-                            finish();
-
+                            fetch_details();
                         } else {
 
                             //verification unsuccessful.. display an error message
